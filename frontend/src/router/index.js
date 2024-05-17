de7afedd859e9515e12a0817fd1b834d5d311f3d
michoubane <michoubane@sqli.com>
2024-05-17 17:01:36 +0100

    configuration de vue-router dans les fichiers main.js et index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import MovieList from '../components/MovieList.vue';
+import MovieDetail from '../components/MovieDetail.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [ 
+        {
+            path: '/',
+            name: 'MovieList',
+            component: MovieList
+        },
+        {
+            path: '/movies/:id',
+            name: 'MovieDetail',
+            component: MovieDetail,
+            props: true
+        }
+    ]
+});
+
+export default router;
