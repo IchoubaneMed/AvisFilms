e53c8575f0ff24207918580bd41772eb3d702803
michoubane <michoubane@sqli.com>
2024-05-22 17:24:16 +0100

    creation d'un url endpoint pour lister les acteurs

--- backend/films/urls.py
+++ backend/films/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MovieListView, MovieDetailView, ReviewCreateView
+from .views import MovieListView, MovieDetailView, ReviewCreateView, ActorListView
 
 
 urlpatterns = [
     path('movies/', MovieListView.as_view(), name='movie_list'),
     path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
     path('movies/<int:id>/review/', ReviewCreateView.as_view(), name='review_create'),
+    path('actors/', ActorListView.as_view(), name='actor_list'),
 ]
 
